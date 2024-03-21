3cddfe9d1bfdf4a99d9b6f6728e465b61255545f
William Fanchiang <wl.fanchiang@gmail.com>
2024-03-21 21:46:00 +0800

    2D list .append & for loop

--- recording.py
+++ recording.py
@@ -14,6 +14,7 @@ while True:
 	l = [name, price] #也可以透過直接將"已定義" str來加入清單之中
 	products2.append([name, price]) #或是透過此方式
 	products1.append(l)
+
 print(products[0][0])
 print(products[0][1])
 print('------------------')
@@ -22,6 +23,11 @@ print(products1[0][1])
 print('------------------')
 print(products2[0][0])
 print(products2[0][1])
+print('------------------')
+for p in products: #為了"p"將"products"清單中拿出來並加以定義"p"；products大清單中有[0]與[1]
+	print(p)
+
+
 
 
 
